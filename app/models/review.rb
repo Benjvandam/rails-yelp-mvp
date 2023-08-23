452acaa6eb97b0ac3e576283e48284e071ff704e
Benjamin Van Dam <benjamin.van.dam@outlook.com>
2023-08-23 16:21:15 -0300

    Finalize app

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,14 @@ class Review < ApplicationRecord
 
   validates :content, presence: true
   validates :rating, presence: true, inclusion: { in: (0..5).to_a }, numericality: { only_integer: true }
+
+  def stars
+    number_stars = ""
+    rating.times do
+      number_stars = "#{number_stars}⭐"
+    end
+
+    return number_stars
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurants = [{ name: "Outback", address: "Botafogo", phone_number: "+32488811450", category: "french" },
+               { name: "McDonalds", address: "Leblon", phone_number: "+55975228532", category: "japanese" },
+               { name: "Burger King", address: "Copacabana", phone_number: "+324999664577", category: "chinese" },
+               { name: "Mays", address: "Antwerpen", phone_number: "+32438577359", category: "chinese" },
+               { name: "Amadeus", address: "Antwerpen", phone_number: "+32404933875", category: "belgian" }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
